2e48722dfefd0885fe22b4ceb971e2e1c14671a4
Denise <88467086+moncadad@users.noreply.github.com>
2026-09-23 16:43:48 -0500

    Update Step3.tsx
    missing braces

--- src/components/Step3/Step3.tsx
+++ src/components/Step3/Step3.tsx
@@ -47,68 +47,66 @@ const Step3 = ({ recipeID }: Step3Props): React.ReactElement | null => {
     };
   }).filter((item) => item.ingredient);
 
-  // if (!recipeID) {
-  //   return (
-  //     // <section id="step3">
-  //     //   <h2>Looks like you haven't made a choice </h2>
-  //     // </section>
-  //     console.log("Nothing selected yet")
-  //   );
-};
+  if (!recipeID) {
+    return null;
+  }
 
-return (
-  <section id="step3">
-    <div className="wrapper mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-      <h1 className="title text-end ">Step 3</h1>
-      <h2 className="subtitle pb-4 text-end">Let's make it! </h2>
-      <div className="row  grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
-        <div className="img-col ">
-          <div className=" rounded-2xl bg-pink-900 ">
-            <img
-              src={recipe?.strDrinkThumb}
-              alt="recipe name"
-              className=" mask-b-from-80% mask-b-to-120% w-lg rounded-2xl"
-            />
+  return (
+    <section id="step3">
+      <div className="wrapper mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <h1 className="title text-end ">Step 3</h1>
+        <h2 className="subtitle pb-4 text-end">Let's make it! </h2>
+        <div className="row  grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 ">
+          <div className="img-col ">
+            <div className=" rounded-2xl bg-pink-900 ">
+              <img
+                src={recipe?.strDrinkThumb}
+                alt="recipe name"
+                className=" mask-b-from-80% mask-b-to-120% w-lg rounded-2xl"
+              />
+            </div>
           </div>
-        </div>
-        <div className="info-col  flex flex-col justify-end py-4">
-          <h1 className="recipe-name text-3xl font-medium md:text-4xl">
-            {recipe?.strDrink}
-          </h1>
-          <h2 className="pb-6 text-xl  md:text-2xl"> {recipe?.strCategory}</h2>
-          <p className="pb-2">Glass: {recipe?.strGlass}</p>
-          <ul className="pb-2">
-            <p>Ingredients:</p>
-            {ingredients.map((item, index) => (
-              <li key={index} className="">
-                {item.measure}
-                {"- "} {item.ingredient}
-              </li>
-            ))}
-          </ul>
-          <div className="pb-4">
-            <p>Instructions:</p>
-            {recipe?.strInstructions}
-          </div>
+          <div className="info-col  flex flex-col justify-end py-4">
+            <h1 className="recipe-name text-3xl font-medium md:text-4xl">
+              {recipe?.strDrink}
+            </h1>
+            <h2 className="pb-6 text-xl  md:text-2xl">
+              {" "}
+              {recipe?.strCategory}
+            </h2>
+            <p className="pb-2">Glass: {recipe?.strGlass}</p>
+            <ul className="pb-2">
+              <p>Ingredients:</p>
+              {ingredients.map((item, index) => (
+                <li key={index} className="">
+                  {item.measure}
+                  {"- "} {item.ingredient}
+                </li>
+              ))}
+            </ul>
+            <div className="pb-4">
+              <p>Instructions:</p>
+              {recipe?.strInstructions}
+            </div>
 
-          <div className="buttons gap-6 md:flex ">
-            <a
-              className="secondary-btn label mb-4 flex w-full items-center justify-center  rounded-3xl px-6 py-2 text-white md:w-auto "
-              href="#step2"
-            >
-              Select another drink
-            </a>
-            <a
-              className="btn primary-btn label mb-4 flex w-full items-center justify-center  rounded-3xl px-6 py-2 text-white md:w-auto"
-              href="#step1"
-            >
-              Let's start again
-            </a>
+            <div className="buttons gap-6 md:flex ">
+              <a
+                className="secondary-btn label mb-4 flex w-full items-center justify-center  rounded-3xl px-6 py-2 text-white md:w-auto "
+                href="#step2"
+              >
+                Select another drink
+              </a>
+              <a
+                className="btn primary-btn label mb-4 flex w-full items-center justify-center  rounded-3xl px-6 py-2 text-white md:w-auto"
+                href="#step1"
+              >
+                Let's start again
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-);
-
+    </section>
+  );
+};
 export default Step3;
